7f9b2da2a8683406c98e7abf21306fb39fce9bbd
Punyansh Singla <punyanshcoder@gmail.com>
2026-05-13 15:32:41 +0530

    added user model

---
+++ server/src/models/User.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    emailVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User =
+  mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
